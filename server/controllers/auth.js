6d991e087d6ccc1a109731fc245e4c6f7cbcfffd
stephanie-ai <stephanie_cooper@hotmail.com>
2021-01-08 16:59:51 +0000

    add routes for register and login

---
+++ server/controllers/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+router.post('/register', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashed = await bcrypt.hash(req.body.password, salt);
+        await User.create({...req.body, password: hashed});
+        res.status(201).json({msg: 'User created'});
+    } catch (err) {
+        res.status(500).json({err});
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findByUsername(req.body.username)
+        if(!user){
+            throw new Error('No user with this username')
+        }
+        const authed = bcrypt.compare(req.body.password, user.passwordDigest);
+        if (!!authed) {
+            res.status(200).json({ user });
+        } else {
+            throw new Error('User could not be authenticated');
+        }
+    } catch (err) {
+        res.status(401).json({ err });
+    }
+})
+
+module.exports = router;
