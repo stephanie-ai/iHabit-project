2c79c60781e5b3820f0970a93673e257d29d91c4
stephanie-ai <stephanie_cooper@hotmail.com>
2021-01-11 09:43:18 +0000

    initial client side set up

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+      return (
+          <main>
+              <h1>Hello World!</h1>
+              <p>Nice to meet you React</p>
+          </main>
+      )
+    }
+  }
+
+export default App;
