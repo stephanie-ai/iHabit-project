1586ac340607d8ad094fcd63e89060cb008a6c63
stephanie-ai <stephanie_cooper@hotmail.com>
2021-01-14 14:57:14 +0000

    habits testing

--- clients/src/pages/Habits/index.js
+++ clients/src/pages/Habits/index.js
@@ -47,7 +47,7 @@ class Habits extends Component {
     render(){
         const renderHabits = this.state.habits.map((p, idx) => <AddHabit key={idx} habits={p} index={idx}/>)
         const newhabit = (<div id = 'newhabit'>
-                <form onSubmit={this.createHabit}>
+                <form id="newhabitForm" onSubmit={this.createHabit}>
                     <label htmlFor="habitName">Habit name:</label>
                     <input type="text" name="habitName" />
 
